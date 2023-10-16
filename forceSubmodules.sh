bced834f4111448d706b496c3792a37c88dcc03e
Kristian <kristian.schultz@uni-rostock.de>
2023-10-16 18:00:09 +0200

    Hot fix for a GIT clone problem with submodules.

---
+++ forceSubmodules.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git submodule update --init
+git submodule add https://github.com/COSPOV/ProWRAS.git external/ProWRAS
+git submodule add https://github.com/COSPOV/LoRAS.git external/LoRAS
+git submodule add https://github.com/Team-TUD/CTAB-GAN.git external/CTAB-GAN
+git submodule update --init
